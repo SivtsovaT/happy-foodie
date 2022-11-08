d7f6e742f030e920a33892b03683f0a08d536a38
user <sivtsovatanya1985@gmail.com>
2022-11-08 19:59:25 +0200

    THF-0003
    - created a ResetPassword component;
    - added the functionality of sending a message to reset the password from firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SplashPage from "./components/splash-page/SplashPage";
 import OnboardingOnePage from "./components/onboarding-pages/OnboardingOnePage";
 import OnboardingTwoPage from "./components/onboarding-pages/OnboardingTwoPage";
 import LoginPage from "./components/login-page/LoginPage";
+import ResetPassword from "./components/reset-password-page/ResetPassword";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="onboard1" element={<OnboardingOnePage/>}/>
                 <Route path="onboard2" element={<OnboardingTwoPage/>}/>
                 <Route path="login" element={<LoginPage/>}/>
+                <Route path="reset" element={<ResetPassword/>}/>
             </Routes>
         </>
     )
