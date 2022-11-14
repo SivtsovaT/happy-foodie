710926be8afa52ee8f9563038b728a7a93893aa7
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2022-11-14 12:59:18 +0200

    Thf 0005 (#5)
    * Updated project:
    
    - clear PR
    
    * THF-0003
    
    - created a ResetPassword component;
    - added the functionality of sending a message to reset the password from firebase
    
    * THF-0005
    
    - created an EnterPhoneNumber component;
    - added phone number validation
    - added register by phone & OTP-verification;
    - clear pr
    -added timer on Otp-screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import OnboardingTwoPage from "./components/onboarding-pages/OnboardingTwoPage";
 import LoginPage from "./components/login-page/LoginPage";
 import ResetPassword from "./components/reset-password-page/ResetPassword";
 import SignUpPage from "./components/sign-up-page/SignUpPage";
+import EnterPhoneNumber from "./components/phone-signup-page/enter-number-page/EnterPhoneNumber";
 
 const App = () => {
     return (
@@ -18,7 +19,7 @@ const App = () => {
                 <Route path="login" element={<LoginPage/>}/>
                 <Route path="reset" element={<ResetPassword/>}/>
                 <Route path="signup" element={<SignUpPage/>}/>
-
+                <Route path="phone" element={<EnterPhoneNumber/>}/>
             </Routes>
         </>
     )
---
+++ src/sign-up-page/SignUpPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './SignUpPage.scss';
+
+const SignUpPage = () => {
+	return (
+		<div>signup</div>
+	)
+}
+
+export default SignUpPage;
